4ad2533c517e1cb98bb77db5e11679ffbde3b489
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-04-05 13:44:09 +0900

    ブログページ書き方修正

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,12 +1,11 @@
-import React from "react";
 import Layout from "../components/Layout";
 import Link from "next/link";
-import getAllPostData from "../lib/post";
+import { getAllPostsData } from "../lib/posts";
 import Post from "../components/Post";
 
-const BlogPage = ({ filteredPosts }) => {
+export default function BlogPage({ filteredPosts }) {
   return (
-    <Layout title="Blog Page">
+    <Layout title="Blog page">
       <ul>
         {filteredPosts &&
           filteredPosts.map((post) => <Post key={post.id} post={post} />)}
@@ -14,16 +13,16 @@ const BlogPage = ({ filteredPosts }) => {
       <Link href="/main-page" passHref>
         <div className="flex cursor-pointer mt-12">
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 mr-3"
+            className="w-6 h-6 mr-3"
             fill="none"
-            viewBox="0 0 24 24"
             stroke="currentColor"
-            strokeWidth="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
+              strokeWidth={2}
               d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
             />
           </svg>
@@ -32,16 +31,11 @@ const BlogPage = ({ filteredPosts }) => {
       </Link>
     </Layout>
   );
-};
-
-export default BlogPage;
-
-export const getStaticProps = async () => {
-  const filteredPosts = await getAllPostData();
-
+}
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostsData();
   return {
-    props: {
-      filteredPosts,
-    },
+    props: { filteredPosts },
+    revalidate: 3,
   };
-};
+}
