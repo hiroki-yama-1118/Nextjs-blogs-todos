1cb3abea5d4ed872baa32d09aa1f9042f55fc744
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-04-05 13:44:54 +0900

    ポストコンポーネントにリンク追加

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Post = ({ post }) => {
   return (
     <div>
       <span>{post.id}</span>
       {" : "}
-      <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
-        {post.title}
-      </span>
+      <Link href={`/posts/${post.id}`} passHref>
+        <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+          {post.title}
+        </span>
+      </Link>
     </div>
   );
 };
