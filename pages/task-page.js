ea3b403bca9ae694499b9c5fc7dea09d96d6eda8
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-04-05 17:05:54 +0900

    タスクページにタスクフォーム追加

--- pages/task-page.js
+++ pages/task-page.js
@@ -5,6 +5,8 @@ import Task from "../components/Task";
 import { getAllTasksData } from "../lib/tasks";
 import useSWR from "swr";
 import { useEffect } from "react";
+import StateContextProvider from "../context/StateContext";
+import TaskForm from "../components/TaskForm";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task`;
@@ -25,33 +27,37 @@ const TaskPage = ({ staticfilteredTasks }) => {
   }, [mutate]);
 
   return (
-    <Layout title="Task Page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <Task key={task.id} task={task} taskDeleted={mutate} />
-          ))}
-      </ul>
-      <Link href="/main-page" passHref>
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 mr-3"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    // この中で好きなようにvalueの値を使用できる
+    <StateContextProvider>
+      <Layout title="Task Page">
+        <TaskForm taskCreated={mutate} />
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page" passHref>
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 };
 
